3249d843af6b950e8aef3f3560f5110ed573e317
sumi-0208 <66242309+sumi-0208@users.noreply.github.com>
2020-09-28 16:16:28 +0900

    変更しました。

--- scrollReveal.js
+++ scrollReveal.js
@@ -1,1 +1,9 @@
-
+ $(function(){
+            ScrollReveal().reveal('.animate');
+            ScrollReveal().reveal('.animateright', { distance: '200px', origin: 'right', viewFactor: '0.8' });
+            ScrollReveal().reveal('.animateleft', { distance: '200px', origin: 'left', viewFactor: '0.8' });
+            ScrollReveal().reveal('.animateslow', { duration: '3000', reset: 'true'});
+            ScrollReveal().reveal('.animatespeed1', { duration: '1800' } );
+            ScrollReveal().reveal('.animatespeed2', { duration: '1800' ,delay: '500'} );
+            ScrollReveal().reveal('.animatespeed3', { duration: '1800' ,delay: '1000'} );
+        });
